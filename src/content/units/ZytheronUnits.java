31fe9c40c6d171fc9e43f7376ef05bcdd958b872
m-e-l-i-s <1234.meils3@gmail.com>
2026-01-20 22:29:24 -0300

    Update ZytheronUnits.java

--- src/content/units/ZytheronUnits.java
+++ src/content/units/ZytheronUnits.java
@@ -12,41 +12,69 @@ public class ZytheronUnits {
 
     public static void load() {
         // region ground attack
-
-        electron = new UnitType("electron") {
-            {
-                constructor = TankUnit::create;
-                researchCostMultiplier = 0.5f;
-                speed = 0.8f;
-                hitSize = 8f;
-                health = 190;
-                weapons.add(new Weapon("electron-turret") {
-                    {
-                        reload = 40f;
-                        x = 0f;
-                        y = 0f;
-                        top = false;
-                        mirror = false;
-                        rotate = true;
-                        bullet = new LightningBulletType() {
-                            {
-                                collides = true;
-                                collidesAir = true;
-                                collidesTiles = true;
-                                pierceCap = 4;
-                                range = 96;
-                                maxRange = 96;
-                                damage = 22;
-                                pierce = true;
-                                lightningLength = 21;
-                                buildingDamageMultiplier = 1;
-                                shootEffect = Fx.lightningShoot;
-                                status = StatusEffects.shocked;
-                            }
-                        };
-                    }
-                });
-            }
-        };
+        electron = new UnitType("electron") {{
+            constructor = TankUnit::create;
+            researchCostMultiplier = 0.5f;
+            speed = 0.8f;
+            hitSize = 8f;
+            health = 190;
+            weapons.add(
+                new Weapon("electron-turret") {{
+                    reload = 40f;
+                    x = 0f;
+                    y = 0f;
+                    top = false;
+                    mirror = false;
+                    rotate = true;
+                    bullet = new LightningBulletType() {{
+                        collides = true;
+                        collidesAir = true;
+                        collidesTiles = true;
+                        pierceCap = 4;
+                        range = 96;
+                        maxRange = 96;
+                        damage = 22;
+                        pierce = true;
+                        lightningLength = 21;
+                        buildingDamageMultiplier = 1;
+                        shootEffect = Fx.lightningShoot;
+                        status = StatusEffects.shocked;
+                    }};
+                }}
+            );
+        }};
+        voltage = new UnitType("voltage") {{
+            constructor = TankUnit::create;
+            researchCostMultiplier = 0.5f;
+            speed = 0.7f;
+            hitSize = 8f;
+            health = 590;
+            weapons.add(
+                new Weapon("electron-turret") {{
+                    reload = 60f;
+                    x = 0f;
+                    y = 0f;
+                    top = false;
+                    mirror = false;
+                    rotate = true;
+                    shoot.shots = 5;
+                    shoot.shotDelay = 8;
+                    bullet = new LightningBulletType() {{
+                        collides = true;
+                        collidesAir = true;
+                        collidesTiles = true;
+                        pierceCap = 4;
+                        range = 96;
+                        maxRange = 96;
+                        damage = 12;
+                        pierce = true;
+                        lightningLength = 21;
+                        buildingDamageMultiplier = 1;
+                        shootEffect = Fx.lightningShoot;
+                        status = StatusEffects.shocked;
+                    }};
+                }}
+            );
+        }};
     }
 }
