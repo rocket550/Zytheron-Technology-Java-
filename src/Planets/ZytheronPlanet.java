da1d39df711491590c4a2222015f039d61696a9d
rocket550 <m.tteo.gatti@gmail.com>
2026-05-09 19:08:28 -0300

    Refactor ZytheronPlanet class and fix errors
    Corrected errors and improved class structure for ZytheronPlanet.

--- src/Planets/ZytheronPlanet.java
+++ src/Planets/ZytheronPlanet.java
@@ -13,29 +13,36 @@ import mindustry.graphics.g3d.PlanetGrid.*;
 import mindustry.maps.planet.*;
 import mindustry.type.*;
 import mindustry.world.*;
+import mindustry.content.*; // Importante para Planets.sun
 import mindustry.world.meta.*;
 
+// ERROR 1 CORREGIDO: Todo debe estar dentro de una clase
+public class ZytheronPlanet {
+    
+    public static Planet zytheron; // Cambiado a minúscula por convención, pero puede ser Zytheron
 
-public static Planet Zytheron;
-
-public static void load(){
-    miPlaneta = new Planet("Zytheron Planets.sun, 1f, 3){{
-        // Generador (debes tener la clase creada)
-        generator = new ZytheronGenerator();
-        
-        // Visuales
-        meshLoader = () -> new HexMesh(this, 5);
-        atmosphereColor = Color.valueOf("32a852"); // Un verde radioactivo
-        iconColor = Color.valueOf("32a852");
-        
-        // Configuración de campaña
-        startSector = 15;
-        alwaysUnlocked = true;
-        
-        // Reglas
-        ruleSetter = rule -> {
-            rule.waveTeam = Team.crux;
-            rule.lighting = true; // Activa luz nocturna
-        };
-    }};
+    public static void load() {
+        // ERROR 2 y 3 CORREGIDOS: Se cerró la comilla, se agregó la coma y se usó el nombre de variable correcto
+        zytheron = new Planet("zytheron", Planets.sun, 1f, 3) {{
+            
+            // Generador (Asegúrate de que la clase ZytheronGenerator exista)
+            generator = new ZytheronGenerator();
+            
+            // Visuales
+            meshLoader = () -> new HexMesh(this, 5);
+            atmosphereColor = Color.valueOf("32a852"); 
+            iconColor = Color.valueOf("32a852");
+            
+            // Configuración de campaña
+            startSector = 15;
+            alwaysUnlocked = true;
+            accessible = true; // Añadido para que puedas clickearlo
+            
+            // Reglas
+            ruleSetter = rule -> {
+                rule.waveTeam = Team.crux;
+                rule.lighting = true; 
+            };
+        }};
+    }
 }
