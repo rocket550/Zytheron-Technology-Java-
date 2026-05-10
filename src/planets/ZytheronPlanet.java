3d3dc61bfc942017126f08a25211a9483bdd67c3
rocket550 <m.tteo.gatti@gmail.com>
2026-05-09 21:21:51 -0300

    Refactor ZytheronPlanet.java with improved comments
    Updated comments and modified water layer parameters.

--- src/planets/ZytheronPlanet.java
+++ src/planets/ZytheronPlanet.java
@@ -15,19 +15,15 @@ public class ZytheronPlanet {
             generator = new ZytheronGenerator();
             
             meshLoader = () -> new MultiMesh(
-                // CAPA 1: El suelo (Piedra, Arena y Fondo Marino)
+                // Capa de tierra: Piedra y Arena
                 new NoiseMesh(this, 15, 6, 1f, 3, 0.5f, 0.4f, 15f, 
                     Color.valueOf("808080"), Color.valueOf("f7e09a"), 4, 0.5f, 0.4f, 0.5f),
                 
-                // CAPA 2: El Agua Transparente
-                // Usamos 0.995f de radio para que esté "pegada" al suelo
-                new NoiseMesh(this, 1, 1, 0.995f, 0, 0f, 0f, 0f, 
-                    Color.valueOf("2b60de").a(0.4f), // Azul con 40% de opacidad
-                    Color.valueOf("1e419b").a(0.4f), 
-                    2, 0.5f, 0.4f, 0.5f)
+                // Capa de agua: Azul transparente
+                new NoiseMesh(this, 1, 1, 0.992f, 0, 0f, 0f, 0f, 
+                    Color.valueOf("2b60de").a(0.4f), Color.valueOf("1e419b").a(0.4f), 2, 0.5f, 0.4f, 0.5f)
             );
 
-            // Atmósfera celeste
             atmosphereColor = Color.valueOf("87ceeb"); 
             atmosphereRadIn = 0.02f;
             atmosphereRadOut = 0.3f;
@@ -37,9 +33,9 @@ public class ZytheronPlanet {
             alwaysUnlocked = true;
             accessible = true;
             
-            ruleSetter = rule -> {
-                rule.waveTeam = Team.crux;
-                rule.lighting = true;
+            ruleSetter = r -> {
+                r.waveTeam = Team.crux;
+                r.lighting = true;
             };
         }};
     }
