ba23e6fc9a7e906572165d304ef2d8ca7140daf6
Trocket <m.tteo.gatti@gmail.com>
2026-06-13 13:42:31 -0300

    Actualizar ZytheronGenerator.java

--- src/planets/ZytheronGenerator.java
+++ src/planets/ZytheronGenerator.java
@@ -16,7 +16,7 @@ public class ZytheronGenerator extends PlanetGenerator {
     
     public Color getColor(Vec3 position) {
         // Usamos una semilla fija (1) o 'seed' si el compilador la reconoce
-        float noise = Simplex.noise3d(1, 7, 0.5, 1/22.0, position.x, position.y, position.z);
+        float noise = Simplex.noise3d(1, 4, 0.5, 1/22.0, position.x, position.y, position.z);
         
         if(noise < -0.1f) return Color.valueOf("1a3a8f"); // Azul Profundo
         if(noise < 0.0f) return Color.valueOf("f7e09a");  // Arena
