2261486743107728885d3d57157221fabd518e32
Trocket <m.tteo.gatti@gmail.com>
2026-06-25 12:51:49 -0300

    Add ZytheronItems class with ZTcoal and ZTcoal2

---
+++ src/content/units/ZytheronItems.java
@@ -0,0 +1,25 @@
+package content.units;
+
+import mindustry.type.Item;
+
+public class ZytheronItems {
+public static Item ZTcoal, ZTcoal2;
+
+    public static void load(){
+        ZTcoal = new Item("ZTcoal"){{
+            alwaysUnlocked = true;
+            explosiveness = 0.2f;
+            charge = 0.0f;
+            flammability = 1f;
+            radioactivity = 0.0f;
+            color = SLPal.starOrangeColor;
+        }};
+        ZTcoal2 = new Item("ZTcoal2"){{
+            alwaysUnlocked = true;
+            explosiveness = 0.1f;
+            charge = 0.0f;
+            flammability = 0.2f;
+            radioactivity = 0.0f;
+        }};
+    }
+}
