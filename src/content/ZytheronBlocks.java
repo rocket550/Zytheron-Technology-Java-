43c4229d8a7cd977a6b04166e1dbd563c0f6a219
Trocket <m.tteo.gatti@gmail.com>
2026-06-29 17:03:05 -0300

    I added a lot of imports, NOW IT SHOULD WORK, right?

--- src/content/ZytheronBlocks.java
+++ src/content/ZytheronBlocks.java
@@ -1,31 +1,44 @@
 package content;
 
-import mindustry.content.Fx;
-import mindustry.content.Items;
-import mindustry.entities.UnitSorts;
-import mindustry.entities.bullet.*;
-import mindustry.entities.part.RegionPart;
-import mindustry.gen.*;
-import mindustry.graphics.Layer;
-import mindustry.world.Block;
-import mindustry.world.blocks.distribution.*;
-import mindustry.world.blocks.defense.Wall;
-import mindustry.world.blocks.defense.turrets.ItemTurret;
-import mindustry.world.blocks.defense.turrets.LiquidTurret;
-import mindustry.world.blocks.environment.OreBlock;
-import mindustry.world.blocks.units.UnitFactory;
-import mindustry.world.blocks.units.Reconstructor;
-import mindustry.world.consumers.*;
 import arc.graphics.*;
-import arc.struct.EnumSet;
-import arc.util.Time;
-import arc.math.Interp;
-import arc.math.Mathf;
+import arc.math.*;
+import arc.struct.*;
+import mindustry.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.DrawPart.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
 import mindustry.type.*;
+import mindustry.type.unit.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.heat.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.payloads.*;
+import mindustry.world.blocks.power.*;
 import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
 import mindustry.world.draw.*;
 import mindustry.world.meta.*;
 
+import static mindustry.Vars.*;
+import static mindustry.type.ItemStack.*;
 
 public class ZytheronBlocks {
     public static Block
