294a7e62a2de5d9fe0c192102dd705098f1dd857
Trocket <m.tteo.gatti@gmail.com>
2026-07-18 03:12:00 -0300

    Rename SLPal to ZytheronColors and update colors
    Refactored color class to ZytheronColors and updated color definitions.

---
+++ src/content/ZytheronColors.java
@@ -0,0 +1,19 @@
+package content;
+
+import arc.graphics.Color;
+
+public class ZytheronColors {
+    public static Color
+
+    greenColor = Color.valueOf("00a340"),
+    greenColorDark = Color.valueOf("005220"),
+    /*silviriumOtherColor = Color.valueOf("88aaff"),
+    silviriumligthColor = Color.valueOf("cceeff"),
+    silviriumDarkestColor = Color.valueOf("222744"),
+    silviriumMidColor = Color.valueOf("00c096"),
+    silviriumDarkColor = Color.valueOf("06745b"),
+    starOrangeColor = Color.valueOf("FF7733"),
+    starOrangeDarkColor = Color.valueOf("FF5511"),
+    starRedColor = Color.valueOf("FF3300"),
+    starRedDarkColor = Color.valueOf("EE1100")*/;
+}
