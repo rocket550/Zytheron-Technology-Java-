0d054fd46a10bdba31fda9ee006abab0d8fe5bea
m-e-l-i-s <1234.meils3@gmail.com>
2026-07-18 03:20:11 -0300

    Update ZytheronColors.java

--- src/content/ZytheronColors.java
+++ src/content/ZytheronColors.java
@@ -6,8 +6,9 @@ public class ZytheronColors {
     public static Color
 
     greenColor = Color.valueOf("00a340"),
-    greenColorDark = Color.valueOf("005220")
-    /*silviriumOtherColor = Color.valueOf("88aaff"),
+    greenColorDark = Color.valueOf("005220");
+    /*/
+    silviriumOtherColor = Color.valueOf("88aaff"),
     silviriumligthColor = Color.valueOf("cceeff"),
     silviriumDarkestColor = Color.valueOf("222744"),
     silviriumMidColor = Color.valueOf("00c096"),
@@ -15,5 +16,6 @@ public class ZytheronColors {
     starOrangeColor = Color.valueOf("FF7733"),
     starOrangeDarkColor = Color.valueOf("FF5511"),
     starRedColor = Color.valueOf("FF3300"),
-    starRedDarkColor = Color.valueOf("EE1100")*/;
+    starRedDarkColor = Color.valueOf("EE1100");
+    //*/
 }
